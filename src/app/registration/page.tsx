15cf0202902ff800be7eba4832372d777c42e798
Tayyab-cs <muhammadtayyab.cs@gmail.com>
2025-05-22 00:28:16 +0500

    datepicker-timepicker

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
-import { z } from "zod";
+import { date, z } from "zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  RegistrationFormData,
-  registrationSchema,
-} from "@/lib/schemas/registrationSchema";
+import { RegistrationFormData, registrationSchema } from "@/schemas/registration.schema";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+
 import {
   Form,
   FormControl,
@@ -17,7 +20,42 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+
+const genderList = [
+  {
+    key: 1,
+    value: 'male',
+    label: 'Male'
+  },
+  {
+    key: 2,
+    value: 'female',
+    label: 'Female'
+  },
+  {
+    key: 3,
+    value: 'other',
+    label: 'Other'
+  }
+];
 
 export default function Registration() {
   // Declarations...
@@ -26,8 +64,9 @@ export default function Registration() {
     defaultValues: {
       username: "",
       email: "",
-      joiningDate: "",
-      department: "",
+      gender: "",
+      dob: new Date,
+      time: new Date
     },
   });
 
@@ -38,7 +77,7 @@ export default function Registration() {
 
   function onSubmit(values: z.infer<typeof registrationSchema>) {
     // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // ✅ This will be type-safe and valkeyated.
     console.log(values);
   }
 
@@ -51,8 +90,8 @@ export default function Registration() {
         <div className="w-1/2 h-1/2 border border-amber-50 rounded p-2 flex flex-col">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className="w-full flex flex-row gap-2">
-                <div className="w-full">
+              <div className="w-full grid grid-cols-12 gap-2">
+                <div className="w-full col-span-6">
                   <FormField
                     control={form.control}
                     name="username"
@@ -67,7 +106,7 @@ export default function Registration() {
                     )}
                   />
                 </div>
-                <div className="w-full">
+                <div className="w-full col-span-6">
                   <FormField
                     control={form.control}
                     name="email"
@@ -77,6 +116,76 @@ export default function Registration() {
                         <FormControl>
                           <Input placeholder="email" {...field} />
                         </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="w-full col-span-6">
+                  <FormField
+                    control={form.control}
+                    name="gender"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Gender</FormLabel>
+                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select gender" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {genderList.map((option) => (
+                              <SelectItem key={option.key} value={option.value}>{option.label}</SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                      </FormItem>
+                    )} />
+                </div>
+
+                <div className="w-full col-span-6">
+                  <FormField
+                    control={form.control}
+                    name="dob"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Date of birth</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-[240px] pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) =>
+                                date > new Date() || date < new Date("1900-01-01")
+                              }
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormDescription>
+                          Your date of birth is used to calculate your age.
+                        </FormDescription>
                         <FormMessage />
                       </FormItem>
                     )}
---
+++ src/schemas/registration.schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const registrationSchema = z.object({
+    username: z
+        .string()
+        .min(2, {
+            message: "Username must be at least 2 characters.",
+        })
+        .max(50),
+    email: z.string().email({ message: "Please enter a valid email address." }),
+    gender: z.string(),
+    dob: z.date({
+        required_error: "A date of birth is required.",
+    }),
+    time: z.date({
+        required_error: "Time is required.",
+    }),
+});
+
+// For frontend form validation
+export type RegistrationFormData = z.infer<typeof registrationSchema>;
